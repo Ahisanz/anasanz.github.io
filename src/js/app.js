e35289d78a4e9b1f003fed235472866612845d1c
Ana Sanz <35330393+Ahisanz@users.noreply.github.com>
2020-09-11 20:34:06 +0200

    add ie only introduction

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,27 @@
 import axios from 'axios';
 
+
+if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+    console.log('heeyy Im IE')
+    document.getElementById('body').innerHTML = "";
+    document.getElementById('body').innerHTML = `
+    <div id="ie-only">
+        <h1>OH NO!</h1>
+        <p>I'm sorry that this website is not fully supported in Internet Explorer</p>
+        <p>but...</p>
+        <p>I can share some information about me</p>
+        <h2>I'm Ana Sanz, Frontend Developer</h2>
+        <img src="images/IMG_20200729_002055_2p.jpg" style="width: auto; height:250px" >
+        <p class="about">Self and quick learner, organized, versatile, curious and dedicated. Passionate about technology innovations, sci-fi, games, books, human history, ecology, gastronomy. Since 2012 working as freelancer on Graphic/WebDesign and Frontend.</p>
+        <p>If you want to know more about me, feel free to download my CV, check my Linkedin or send me a email</p>
+        <a href="images/cvi_ana_sanz_en.pdf">Download CV</a>
+        <a href="https://www.linkedin.com/in/ana-claudia-sanz/">Linkedin</a>
+        <a href="mailto: anaclaulp@gmail.com">anaclaulp@gmail.com</a>
+        <h2>Thank you! Have a nice day!</h2>
+    </div>
+    `;
+}
+
 // TRANSLATE INTRO ON CLICK
 var devToggle = document.getElementById('dev-toggle');
 var photoToggle = document.getElementById('photo-toggle');
